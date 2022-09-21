3daa9a109f2bc0fedec0ee576eec6725ee4d4476
imadhui <imadhui.cs@gmail.com>
2022-09-21 14:12:18 +0530

    Create linux-setup.sh

---
+++ linux-setup.sh
@@ -0,0 +1,7 @@
+apt update
+apt upgrade
+apt install tldr
+wget https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megacmd-xUbuntu_22.04_amd64.deb
+apt install ./megacmd-xUbuntu_22.04_amd64.deb
+mega-login imadhui.cs@gmail.com qomDak-gytqe0-juhtop
+
